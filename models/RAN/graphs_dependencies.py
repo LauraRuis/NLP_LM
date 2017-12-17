a80c7bd039b7e92e6d80c8ced345323fd5589e51
LauraRuis <lauraruis@live.nl>
2017-12-17 14:48:01 +0100

    updated graphs

--- models/RAN/graphs_dependencies.py
+++ models/RAN/graphs_dependencies.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def medians_per_sentence_length(data, groups, normalized=True):
+def medians_per_sentence_length(data, groups, normalized=False):
 
     medians = []
     for key in groups:
@@ -21,49 +21,81 @@ def medians_per_sentence_length(data, groups, normalized=True):
 
 def grouped_barchart(data1, data2, data3):
 
-    groups = sorted([int(d) for d in list(data1.keys())])
-    groups = np.array(groups)
-    medians_data1 = medians_per_sentence_length(data1, groups)
-    medians_data2 = medians_per_sentence_length(data2, groups)
-    medians_data3 = medians_per_sentence_length(data3, groups)
+    # Setting the positions and width for the bars
+    pos = [i for i in range(2, 55, 5)]
+    width = 1
 
-    fig, ax = plt.subplots()
-    bar_width = 0.35
+    medians_data1 = medians_per_sentence_length(data1, pos)
+    medians_data2 = medians_per_sentence_length(data2, pos)
+    medians_data3 = medians_per_sentence_length(data3, pos)
 
-    opacity = 0.4
-    error_config = {'ecolor': '0.3'}
+    # Plotting the bars
+    fig, ax = plt.subplots(figsize=(10, 5))
 
-    rects1 = plt.bar(groups - bar_width, medians_data1, bar_width,
-                     alpha=opacity,
-                     color='b',
-                     error_kw=error_config,
-                     label='max')
+    # Create a bar with pre_score data,
+    # in position pos,
+    plt.bar(pos,
+            # using df['pre_score'] data,
+            medians_data1,
+            # of width
+            width,
+            # with alpha 0.5
+            alpha=1,
+            # with color
+            # with label the first value in first_name
+            label='max')
 
-    rects2 = plt.bar(groups, medians_data2, bar_width,
-                     alpha=opacity,
-                     color='r',
-                     error_kw=error_config,
-                     label='sum')
+    # Create a bar with mid_score data,
+    # in position pos + some width buffer,
+    plt.bar([p + width for p in pos],
+            # using df['mid_score'] data,
+            medians_data2,
+            # of width
+            width,
+            # with alpha 0.5
+            alpha=1,
+            # with color
+            # with label the second value in first_name
+            label='sum')
 
-    rects2 = plt.bar(groups + bar_width, medians_data3, bar_width,
-                     alpha=opacity,
-                     color='g',
-                     error_kw=error_config,
-                     label='l2')
+    # Create a bar with post_score data,
+    # in position pos + some width buffer,
+    plt.bar([p + width * 2 for p in pos],
+            # using df['post_score'] data,
+            medians_data3,
+            # of width
+            width,
+            # with alpha 0.5
+            alpha=1,
+            # with color
+            # with label the third value in first_name
+            label='l2')
 
-    plt.xlabel('Sentence Length')
-    plt.ylabel('Normalized Dependency Distance')
-    plt.title('Word Dependencies Per Sentence Length by 1-layer RAN\nNormalized By Sentence Length')
-    plt.xticks(groups + bar_width / 2, tuple([str(i) for i in groups]))
+    # Set the y axis label
+    ax.set_ylabel('Dependency Lengths')
+    ax.set_xlabel('Sentence Lengths')
+
+    # Set the chart's title
+    ax.set_title('Median Dependency Length per Different Sentence Length of One Layer RAN')
+
+    # Set the position of the x ticks
+    ax.set_xticks([p + 1.5 * width for p in pos])
+
+    # Set the labels for the x ticks
+    ax.set_xticklabels([str(i) for i in pos])
+
+    # Setting the x-axis and y-axis limits
+    plt.xlim(min(pos) - width, max(pos) + width * 4)
+
+    # Adding the legend and showing the plot
     plt.legend()
-
-    plt.tight_layout()
+    ax.yaxis.grid()
     plt.show()
 
 
-pickle_location_max = 'dependencies_penn1_max/'
-pickle_location_sum = 'dependencies_penn1_sum/'
-pickle_location_l2 = 'dependencies_penn1_l2/'
+pickle_location_max = 'average_max/'
+pickle_location_sum = 'average_sum/'
+pickle_location_l2 = 'average_l2/'
 files_penn_l2 = [
     'all_max_l2_penn_one_layer',
     'all_med_l2_penn_one_layer',
@@ -86,5 +118,8 @@ files_penn_max = [
 max_data = load_obj(pickle_location_max + files_penn_max[0])
 sum_data = load_obj(pickle_location_sum + files_penn_sum[0])
 l2_data = load_obj(pickle_location_l2 + files_penn_l2[0])
-print(max_data)
+for key, value in max_data.items():
+    print(key)
+    print(sorted(value))
+    print()
 grouped_barchart(max_data, sum_data, l2_data)
